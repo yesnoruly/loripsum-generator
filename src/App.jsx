d4b3fabdd194ea5c226abe9fcc4b56133e04bdec
Yesnoruly <64963734+Karvacky-Roma@users.noreply.github.com>
2021-06-14 21:22:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//Styles
+import './App.scss'
+
+export const App = () => {
+
+    return (
+        <div className="app">
+
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Style
+import './index.scss';
+//Component
+import {App} from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
