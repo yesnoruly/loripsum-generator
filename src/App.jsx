e425d21072dfc4eacf6dcbbc5dcda80014871936
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-07-07 16:01:03 +0300

    Removed the Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 //Styles
 import './App.scss'
 //Components
-import {Form} from './Component/Form/Form'
 import {Output} from "./Component/Output/Output";
 import {Paragraph} from "./Component/Paragraph/Paragraph";
 import {Empty} from "./Component/Empty/Empty";
@@ -11,6 +10,8 @@ import {CopyToClipboard} from './Component/CopyToClipboard/CopyToClipboard'
 //Effector
 import {$loripsum, fetchLoripsumDataFx, $joinedLoripsum} from './effector';
 import {useStore} from 'effector-react'
+import {CounterInput} from "./Component/CounterInput/CounterInput";
+import {Button} from "./Component/Button/Button";
 
 export const App = () => {
 
@@ -18,12 +19,26 @@ export const App = () => {
 	const isLoading = useStore(fetchLoripsumDataFx.pending)
 	const joinedLoripsum = useStore($joinedLoripsum)
 
+	const [input, setInput] = useState("1")
+
+	const handleSubmit = (evt) => {
+		evt.preventDefault();
+		fetchLoripsumDataFx(input);
+	}
+
 	return (
 		<div className="app loripsum">
 
 			<h2 className="title loripsum__title">Tired of boring lorem ipsum?</h2>
 
-			<Form/>
+			<form onSubmit={handleSubmit} className="form loripsum__form">
+				<CounterInput value={input} onChange={e => setInput(e.target.value)} labelText="Paragraphs:"/>
+
+				<Button
+					buttonType="submit"
+					buttonText="Generate"
+					className="loripsum__button"/>
+			</form>
 
 			<Output className="loripsum__output">
 
