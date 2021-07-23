599347994bbcc9ce09f9130fb4b7fe29a1cae8d3
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-07-23 12:50:30 +0300

    Removed unnecessary imports

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 //Styled
-import styled, {keyframes, css} from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 export const Loader = (props) => {
     return (
