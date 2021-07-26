93912516c203794bd6743c5bb7eaee628eb0dfa8
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-07-26 10:36:52 +0300

    Created Form.jsx

---
+++ src/Components/Form.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+//Styled
+import styled, {css} from 'styled-components';
+
+export const Form = (props) => {
+    return <StyledForm {...props}>{props.children}</StyledForm>
+}
+
+const StyledForm = styled.form`
+    margin-top: ${props => props.$mtop || "0"};
+
+    ${props => props.$grid && css`
+        display: grid;
+        grid-auto-flow: ${props => props.$auto_flow || "column"};
+        justify-content: ${props => props.$jcontent || "center"};
+        align-items: ${props => props.$jcontent || "center"};
+        grid-gap: ${props => props.$gap || "0"};
+
+        @media (max-width: 362px) {
+            grid-auto-flow: row;
+        }
+    `}
+`
