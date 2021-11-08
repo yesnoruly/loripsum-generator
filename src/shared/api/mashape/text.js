57528f0285b260a80906577934f9e5fc57227e19
Yesnoruly <64963734+yesnoruly@users.noreply.github.com>
2021-11-08 20:56:11 +0200

    refactor: move constants in the base.js

--- src/shared/api/mashape/text.js
+++ src/shared/api/mashape/text.js
@@ -1,11 +1,4 @@
-const API_URL = 'https://mashape-community-skate-ipsum.p.rapidapi.com';
-const OPTIONS = {
-  method: 'GET',
-  headers: {
-    'x-rapidapi-key': 'd0db3facf7msh4bd8b090d719809p1008bdjsn138563af4356',
-    'x-rapidapi-host': 'mashape-community-skate-ipsum.p.rapidapi.com',
-  },
-};
+import {API_URL, OPTIONS} from './base';
 
 export const getTextData = (nums) => {
   return fetch(`${API_URL}${nums}/0/JSON`, OPTIONS)
