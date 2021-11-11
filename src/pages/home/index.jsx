eff8469c495cdb1f3b7385732e352daf16e0eb46
Yesnoruly <64963734+yesnoruly@users.noreply.github.com>
2021-11-11 14:07:45 +0200

    feat: create homepage

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+// Components
+import { CopyToClipboard } from '../../features/copy-to-clipboard';
+import { InputNumber } from '../../shared/ui';
+import { Button } from '../../shared/ui';
+import { Title } from '../../shared/ui';
+import { Output } from '../../shared/ui';
+import { Form } from '../../shared/ui';
+import { Loader } from '../../shared/ui';
+import { Paragraph } from '../../shared/ui';
+
+export const Home = () => {
+  const textContent = useStore($textContent);
+  const isLoading = useStore(getTextContentFx.pending);
+  const textContentJoined = useStore($textContentJoined);
+
+  const [input, setInput] = useState('1');
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    getTextContentFx(input);
+  };
+
+  return (
+    <div className="app">
+
+      <Title $alignment="center" $color="var(--color-blue-darker)">Tired of boring lorem ipsum?</Title>
+
+      <Form onSubmit={handleSubmit} $marginTop="31px" $grid $gap="12px">
+
+        <InputNumber
+          labelText="Paragraphs:"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          min={1}
+          max={999}
+          $color="var(--color-blue-darker)"
+        />
+
+        <Button
+          type="submit"
+          $color="#FFFFFF"
+          $background="var(--color-blue-medium)"
+          $backgroundHover="var(--color-blue-lightest)"
+          $backgroundActive="var(--color-blue-darker)"
+        >
+          Generate
+        </Button>
+      </Form>
+
+      <Output $padding="37px" $marginTop="24px" $height="550px" $gap="32px" $center>
+
+        <CopyToClipboard data={textContentJoined} />
+
+        {
+          isLoading
+            ? <Loader $width="5px" $color="var(--color-blue-light)" />
+            : (
+              textContent.length > 0
+                ? textContent.map((item, index) => (
+                  <Paragraph
+                    key={index}
+                    $center
+                    $size="20px"
+                    $color="var(--color-blue-dark)"
+                  >
+                    {item}
+                  </Paragraph>
+                ))
+                : <p className="empty">😎</p>
+            )
+        }
+
+      </Output>
+
+    </div>
+  );
+};
