68b3b1ff536f3ccc40c2f0643e3356cc84665759
Yesnoruly <64963734+yesnoruly@users.noreply.github.com>
2021-11-11 14:08:27 +0200

    feat: using homepage

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 // Styles
 import './index.css';
+import { Home } from '../pages/home'
 
 export const App = () => {
-  const text = 'asdfjkl;';
-
   return (
-    <p>{text}</p>
+    <Home />
   )
 }
