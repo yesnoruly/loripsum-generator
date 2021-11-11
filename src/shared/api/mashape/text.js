9967baa7fae8b2eae9079fc37c496d6d0bbc0ffd
Yesnoruly <64963734+yesnoruly@users.noreply.github.com>
2021-11-11 15:52:49 +0200

    fix: add a forgotten backwards slash

--- src/shared/api/mashape/text.js
+++ src/shared/api/mashape/text.js
@@ -1,6 +1,6 @@
 import {API_URL, OPTIONS} from './base';
 
 export const getTextContent = (nums) => {
-  return fetch(`${API_URL}${nums}/0/JSON`, OPTIONS)
+  return fetch(`${API_URL}/${nums}/0/JSON`, OPTIONS)
     .then((req) => req.json());
 }
