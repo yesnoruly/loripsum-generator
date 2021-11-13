e382e1a8839833e293a87cff7a722963cefafdac
Yesnoruly <64963734+yesnoruly@users.noreply.github.com>
2021-11-13 20:57:20 +0200

    refactor: delete entities text

--- src/entities/text/model.js
+++
@@ -1,9 +0,0 @@
-import { createStore, createEffect } from 'effector';
-import { mashapeApi } from '../../shared/api/';
-
-export const getTextContentFx = createEffect((num = 1) => mashapeApi.getTextContent(num));
-
-export const $textContent = createStore([])
-  .on(getTextContentFx.doneData, (_, data) => data);
-
-export const $textContentJoined = $textContent.map((arr) => arr.join(' '));
