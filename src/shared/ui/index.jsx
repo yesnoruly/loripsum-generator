3099b607583f2856d98a7eae96e3627b1d4c45e9
Yesnoruly <64963734+yesnoruly@users.noreply.github.com>
2021-11-13 21:57:38 +0200

    refactor: import empty

--- src/shared/ui/index.jsx
+++ src/shared/ui/index.jsx
@@ -5,3 +5,4 @@ export { Loader } from './loader';
 export { Output } from './output';
 export { InputNumber } from './input-number';
 export { Form } from './form';
+export { Empty } from './empty';
