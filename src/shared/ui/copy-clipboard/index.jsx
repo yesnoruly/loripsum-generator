0cfae035f198dfb25ace66f54424a94cdc55c21e
Yesnoruly <64963734+yesnoruly@users.noreply.github.com>
2021-11-17 15:31:13 +0200

    refactor: move copy-clipboard to `shared/ui`

---
+++ src/shared/ui/copy-clipboard/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import copy from './assets/copy.svg';
+import checkmark from './assets/checkmark.svg';
+
+export const CopyToClipboard = ({ isCopied, setCopied }) => (
+    <StyledCopyToClipboard onClick={setCopied}>
+      <img src={isCopied ? checkmark : copy} alt="" />
+    </StyledCopyToClipboard>
+);
+
+const StyledCopyToClipboard = styled.button`
+  background-color: var(--color-grey-lightest);
+  border-radius: 4px;
+  border: 1px solid var(--color-grey-light);
+  padding: 5px;
+  margin: 5px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: background-color .3s ease;
+  &:hover {
+    background-color: var(--color-grey-medium);
+  }
+  &:active {
+    background-color: var(--color-grey-dark);
+  }
+  & img {
+    width: 25px;
+    height: 25px;
+  }
+`;
