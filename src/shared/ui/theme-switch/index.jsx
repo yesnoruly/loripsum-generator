8a0cac90570bc02de81e4f436259da4bec637bff
Karvacky-Roma <64963734+yesnoruly@users.noreply.github.com>
2021-12-12 13:29:06 +0200

    chore: update styling (reset)

--- src/shared/ui/theme-switch/index.jsx
+++ src/shared/ui/theme-switch/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // Assets
 import sun from './assets/sun.svg';
 import moon from './assets/moon.svg';
+// Styles
+import s from './style.module.css'
 
 export const ThemeSwitch = ({
   state,
   handleClick,
 }) => (
-  <button type="button" onClick={handleClick}>
+  <button className={s.button} type="button" onClick={handleClick}>
     <img src={state ? sun : moon} alt="" width="30" height="30" />
   </button>
 );
